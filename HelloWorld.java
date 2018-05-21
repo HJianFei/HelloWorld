f3ad294768cf990ff61c0aae607ff0e9422c2981
HJianFei <190766172@qq.com>
2018-05-21 16:01:20 +0800

    更新代码

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,14 +1,3 @@
-package com.hjianfei.rover.adapter;
-
-/**
- * <pre>
- *     author : HJFei
- *     e-mail : 190766172@qq.com
- *     time   : 2018/05/21
- *     desc   :
- *     version: 1.0
- * </pre>
- */
 public class HelloWorld {
 	
 	public static void main(String[]args){
